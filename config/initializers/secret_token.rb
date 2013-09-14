26007030e2f7ec487f6d1af4d51695d930020918
John Johnson <dampboxdev@gmail.com>
2013-09-13 19:38:40 -0700

    Code Fellows Tutorial-TwitterHolder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetholder::Application.config.secret_key_base = 'b69ec7ed76abe9da5db9cdf88fd940c94495cb0043784290a382a0465f481a9ae7f9ba2010c351beb9f37f2e8e5053abe0871da37d47da9d66ffd1c8d38f04dc'
